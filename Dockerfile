3c953c1a993923c7032932268e4cd5c48def0146
Tucker Kern <tuckkern@gmail.com>
2023-07-15 19:40:00 -0600

    Add a custom entrypoint to symlink the RTL device to a compatible path for rtl_433

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM hertzg/rtl_433
-RUN apk add --update tini python3 py3-pip
+RUN apk add --update tini python3 py3-pip eudev
 RUN pip3 install paho-mqtt
 COPY rtl-433-mqtt-cmd.py /usr/bin/rtl-433-mqtt-cmd
+COPY entrypoint.sh /usr/bin/entrypoint
 ENV HOST="127.0.0.1"
 ENV OPTIONS=""
 ENV MQTT_USERNAME=""
 ENV MQTT_PASSWORD=""
-ENTRYPOINT ["/sbin/tini", "--"]
+ENV RTL_DEVICE=""
+ENTRYPOINT ["/usr/bin/entrypoint"]
 CMD ["/bin/sh", "-c", "/usr/bin/rtl-433-mqtt-cmd --host $HOST $OPTIONS"]
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# Custom entrypoint to symlink $RTL_DEVICE to rtl_433 friendly path
+# Modified from https://gist.github.com/gwisp2/b68b0d7ecf8ec35a4d1ee7a9ea8552b8
+
+if [ ! -z "$RTL_DEVICE" ]; then
+    
+  if [ ! -e "$RTL_DEVICE" ]; then
+    echo "Device $RTL_DEVICE does not exist."
+    exit 1
+  fi
+
+  # rtl_433 is only capable of finding device under /dev/bus/usb/XXX/YYY
+  # So create a symlink from $RTL_DEVICE to /dev/bus/usb/XXX/YYY, where XXX and YYY match the host
+  # See https://github.com/hertzg/rtl_433_docker/issues/14 for the discussion.
+  USB_BUS_PATH="/dev/$(udevadm info "--name=$RTL_DEVICE" -q name)"
+
+  if [ -z "$USB_BUS_PATH" ]; then
+    echo "Could not find device via udev."
+    exit 1
+  fi
+
+  if [ "$RTL_DEVICE" != "$USB_BUS_PATH" ] && [ ! -f "$USB_BUS_PATH" ]; then
+      mkdir -p "$(dirname "$USB_BUS_PATH")"
+      ln -s "$RTL_DEVICE" "$USB_BUS_PATH"
+  fi
+else
+  echo "No RTL_DEVICE set. No symlink created."
+fi
+
+# Call tini with original command
+exec /sbin/tini -- "$@"
